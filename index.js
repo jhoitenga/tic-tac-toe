d892956d7f84b4bd302515f59ef3de4d18c65489
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-05-29 06:47:54 -0500

    adding additional code

---
+++ index.js
@@ -0,0 +1,38 @@
+const App = {
+    $: {
+        menu: document.querySelector('[data-id="menu"]'),
+        menuItems: document.querySelector('[data-id="items"]'),
+        resetBtn: document.querySelector('[data-id="reset-btn"]'),
+        newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+        squares: document.querySelectorAll('[data-id="square"]'),
+    },
+
+    init() {
+        App.registerEventListeners();
+    },
+
+    registerEventListeners() {
+        App.$.menu.addEventListener("click", (event) => {
+            App.$.menuItems.classList.toggle("hidden");
+        });
+
+        App.$.resetBtn.addEventListener("click", (event) => {
+            console.log('Reset the game');
+        })
+
+        App.$.newRoundBtn.addEventListener("click", (event) => {
+            console.log('Add new round');
+        })
+
+        App.$.squares.forEach(square => {
+            square.addEventListener("click", (event) => {
+                console.log(`Square with id ${event.target.id} was clicked`);
+        });
+    });
+
+    }
+};
+
+window.addEventListener("load", App.init);
+
+
